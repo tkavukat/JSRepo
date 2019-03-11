ca040d9ef37773632d39d65721264db214a73902
THOMAS KAVUKAT <tkavukat@gmail.com>
2019-03-10 22:31:19 -0400

    Slide 13

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const validator = require('validator')
+
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const myNotes = getNotes()
 
 console.log(myNotes)
 
-console.log(validator.isURL('rxnt.com'))
 
+//Use chalk library
+// Install version 2.4.1 of hcalk
+// Load chalk into app.js
+// Use chalk to print the string "Success" to the console in green
+// Test the work
+// Make test bold and inversed
+
+const boldinverse= chalk.green.bold.inverse;
+
+console.log(chalk.green('Success!'))
+console.log(boldinverse('My highlighted bold inverse string'))
