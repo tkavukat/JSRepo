534693fffeb131488abbc988bf7a64a87fb50fa1
THOMAS KAVUKAT <tkavukat@gmail.com>
2019-03-17 06:27:14 -0400

    Chapter 18-1

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,17 @@
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
-
-
 const fs = require('fs')
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+
+//Challenge: Work with JSON and the file system
+//Load and Parse the JSON data
+// Change the name and age property using your info
+//stringify the changed object and overwrite the original data
+//Test
+
+const chalDataBuffer = fs.readFileSync('1-json.json')
+const chalDataString = chalDataBuffer.toString()
+const chalData = JSON.parse(chalDataString)
+chalData.name = 'Resmi G'
+chalData.age = '36'
+
+const chalDataStringToWrite = JSON.stringify(chalData)
+fs.writeFileSync('1-json.json',chalDataStringToWrite)
+
