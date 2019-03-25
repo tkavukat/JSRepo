96e4c5735801b3efb9012118ea438b60fbb68f5d
Thomas K <tkavukat@gmail.com>
2019-03-25 15:42:09 -0400

    Callback abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,14 @@
 const request = require ('request')
 const chalk = require('chalk')
+const geocode = require('./utils/geocode')
 
-const url = 'https://api.darksky.net/forecast/8592531de454db2c8476bae3b11ed3f5/37.8267,-122.4233?lang=es'
-
-// Goal: Print a small forecast to the user
-//
-// 1. Print" It is currently <> degrees out. There is a <>% chance of rain
-// 2. Test the app.
-
-request({url:url, json:true},(error,response)=>{
-    const data = response.body
-    console.log(data.daily.data[0].summary + 
-        ' It is currently ' + data.currently.temperature + ' degrees out. There is a ' 
-        + data.currently.precipProbability + '% chance of rain.')
-})
-
-
+geocode('1234',(error,response) => {
+    if(error){
+        console.log('Error:' + error)
+    }
+    else{
+        console.log('Longitude:' + response.longitude)
+        console.log('Longitude:' + response.latitude)
+    }
+}
+)
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require ('request')
+
+const geocode = (place,callback) => {
+    const mapURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(place) + '.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g&cachebuster=1553279097410&types=place&limit=1'
+    request({url:mapURL,json:true},(error,response)=>{
+        if(error){
+            callback('No connection available',undefined)
+            
+        } else if(response.errCode || response.body.error || response.body.features.length < 1){
+            callback('An error occured, please retry with a new location',undefined)
+        }
+        else{
+            const data = response.body
+            callback(undefined,{
+                    longitude: data.features[0].center[0],
+                    latitude: data.features[0].center[1]
+            })
+        }
+    })
+}
+
+module.exports = geocode
