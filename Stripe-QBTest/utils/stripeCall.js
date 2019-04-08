098ab250d55b1448d60d33295b847e3fe56db355
THOMAS KAVUKAT <tkavukat@gmail.com>
2019-04-07 23:33:04 -0400

    stripe invoice retrieval

--- Stripe-QBTest/utils/stripeCall.js
+++ Stripe-QBTest/utils/stripeCall.js
@@ -1,20 +1,33 @@
-const stripe = require("stripe")("sk_test_LFXMj1CxixZVsgcMHH7ZaBt1");
+const stripe = require("stripe")
 
-const stripeInvoice = (key,callback)=>{
-    stripe.invoices.retrieve(
-        key,
-        (err, invoice) => {
-          // asynchronously called
-            if(err){
-                
-            }
-            else{
-                //callback
-            }
-        }
-      );
+const Invoice = (key,callback)=>{
+  const invoice = 'in_1EM1zJLLmG3lvb4lqI5yk6AP'
+  var stripe = require("stripe")("sk_test_LFXMj1CxixZVsgcMHH7ZaBt1");
+
+  stripe.invoices.retrieve(
+      invoice,
+      (err, invoice) => {
+        // asynchronously called
+          if(err){
+              callback(err,undefined)
+          }
+          else{
+              //callback
+              callback(undefined,invoice)
+          }
+      }
+    );
 }
   
+const stripeCall = (key) => {
+  //get the invoice
 
+  //then get the subscription and product details separately
+
+  //build the json for QB Online
+
+}
+
+module.exports = stripeInvoice
 
 
